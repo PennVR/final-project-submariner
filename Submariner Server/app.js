80f616b8ab05333515f6b54d56fa6d00357889ab
Tim Clancy <clancyt@seas.upenn.edu>
2017-03-24 21:26:47 -0400

    Initial commit of the Submariner project; basic room, Interactable object highlighting, Interaction interface, and Socket.io server with response handling for different objects. Basic mouse, HUD, gamemode, and gamepad control is also setup.

---
+++ Submariner Server/app.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var util = require('util');
+var clients = [];
+
+io.on('connection', function(socket) {
+	clients.push(socket.id);
+	var clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+	console.log(clientConnectedMsg);
+	
+	// Disconnect event
+	socket.on('disconnect', function(data) {
+		clients.pop(socket.id);
+		var clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+		console.log(clientDisconnectedMsg);
+	});
+	
+	socket.on('button', function(data) {
+		console.log(data);
+	});
+	
+	socket.on('lever', function(data) {
+		console.log(data);
+	});
+});
+
+
+http.listen(3000, function(){
+	console.log('Submariner server listening on port 3000.');
+});
