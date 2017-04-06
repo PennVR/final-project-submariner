adf8f727361f5497e3668cb5d3e26ecf0c32f94b
Santiago Buenahora <santib97@gmail.com>
2017-04-05 21:16:31 -0400

    crank + level + handle

--- Submariner Server/app.js
+++ Submariner Server/app.js
@@ -4,6 +4,11 @@ var io = require('socket.io')(http);
 var util = require('util');
 var clients = [];
 
+var buttonPress = false;
+var leverPull = false;
+var crankTurn = false;
+var handlePull = false;
+
 io.on('connection', function(socket) {
 	clients.push(socket.id);
 	var clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
@@ -18,18 +23,43 @@ io.on('connection', function(socket) {
 	
 	socket.on('button', function(data) {
 		console.log(data);
+		buttonPress = true;
 	});
 	
-	socket.on('lever', function(data) {
+	socket.on('engine lever', function(data) {
 		console.log(data);
+		leverPull = true;
 	});
 
 	socket.on('handle', function(data) {
 		console.log(data);
 	});
+
+	socket.on('navigator crank', function(data) {
+		console.log(data);
+		crankTurn = true;
+	});
+
+	socket.on('gunner handle', function(data) {
+		console.log(data);
+		handlePull = true;
+	});
 });
 
+var gameOver = false;
 
-http.listen(3000, function(){
+//win = captain button, gunner handle, engine lever, navigator crank
+
+var loop = function() {
+	if (buttonPress && leverPull && crankTurn && handlePull && !gameOver) {
+		gameOver = true;
+		console.log("sending win");
+		io.sockets.emit('win', { text: 'You win!' });
+	}
+};
+
+setInterval(loop, 1000);
+
+http.listen(3000, function() {
 	console.log('Submariner server listening on port 3000.');
 });
