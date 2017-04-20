3b3713fede5f5cd4b2a86c37c9213dfcf0c7e618
Tim Clancy <clancyt@seas.upenn.edu>
2017-04-20 03:56:50 -0400

    Completed initial final beta prototype.

--- Submariner Server/app.js
+++ Submariner Server/app.js
@@ -14,7 +14,7 @@ var life = 3;
 var buttonPress = false;
 var leverPull = 0;
 var crankTurn = 0;
-var handlePull = "red";
+var handlePull = "Red";
 
 // Character selection variables
 var captainSelect = false;
@@ -28,12 +28,12 @@ var gameOver = false;
 var globalseconds = 0;
 var taskseconds = 0;
 var taskOver = true;
+var startDelay = 70;
 
 // Task variables
-
 var lowerBoundDirection = 30;
 var upperBoundDirection = 80;
-var torpedoColor = "red";
+var torpedoColor = "Red";
 var timeLimit = 600;
 var lowerBoundDepth = 30;
 var upperBoundDepth = 80;
@@ -75,6 +75,21 @@ io.on('connection', function(socket) {
 	socket.on('gunner handle', function(data) {
 		console.log(data);
 		handlePull = data;
+		
+		// Check for victory
+		if (leverPull <= upperBoundDepth && leverPull >= lowerBoundDepth
+			&& crankTurn  <= upperBoundDirection && crankTurn >= lowerBoundDirection
+			&& handlePull.localeCompare(torpedoColor) && taskseconds <= timeLimit && !gameOver) {
+			
+			// If correct, proceed to next task.
+			taskOver = true;
+			taskseconds = 0;
+			console.log("Task succeeded!");
+		} else {
+			
+			// Task corrected incorrectly.
+			issueStrike();
+		}
 	});
 
 	// Character selection events
@@ -109,36 +124,33 @@ io.on('connection', function(socket) {
 var loop = function() {
 
 	// Check for game start (all characters selected)
+	/*
 	if (captainSelect && gunnerSelect && 
 			navigatorSelect && engineerSelect) {
 		gameStart = true;
 	}
-
+	*/
+	if (captainSelect) {
+		startDelay--;
+	}
+	
+	if (startDelay <= 0) {
+		gameStart = true;
+	}
+	
 	if (gameStart) {
 	
-		// Generate a task
+		// Generate a task.
 		if (taskOver) {
-
 			taskOver = false;
-
 			generateTask(function(state) {
 				console.log(state);
+				taskParser(state.state);
 			});
-
-			// TODO: change values for task
 		}
 	
-		// Check for victory
-		if (leverPull <= upperBoundDepth && leverPull >= lowerBoundDepth
-			&& crankTurn  <= upperBoundDirection && crankTurn >= lowerBoundDirection
-			&& handlePull.localeCompare(torpedoColor)  && taskseconds <= timeLimit && !gameOver) {
-			
-			taskOver = true;
-			taskseconds = 0;
-			console.log("task finished");
-		}
-
-		if (globalseconds >= 3600 && !gameOver) {
+		// Check for game end.
+		if (globalseconds >= 1200 && !gameOver) {
 			console.log("sending win");
 			io.sockets.emit('win', { text: 'You win!' });
 			gameOver = true;
@@ -149,25 +161,16 @@ var loop = function() {
 
 		// Check for defeat
 		if (taskseconds >= timeLimit && !gameOver) {
-			life--;
-			if (life == 0) {
-				gameOver = true;
-				console.log("sending lose");
-				io.sockets.emit('lose', { text: 'You lose!' });
-
-				// Reset the game
-				resetGame();
-			} else {
-				taskOver = true
-				taskseconds = 0;
-				console.log("task time out");
-				io.sockets.emit('health_down',{ text: 'Wrong!  Your health now goes down by one.'});
-				console.log("health down");
-			}
-
+			issueStrike();
 		}
 
-		// Increment timer
+		// Emit current depth.
+		io.sockets.emit('current depth', leverPull);
+		
+		// Emit current direction.
+		io.sockets.emit('current direction', crankTurn);
+		
+		// Handle timers
 		taskseconds++;
 		globalseconds++;
 	}
@@ -189,6 +192,28 @@ var resetGame = function() {
 	gameStart = false;
 	gameOver = false;
 	deciseconds = 0;
+	
+	life = 4; // Fuck this bug TODO
+};
+
+// Issue a strike to the players.
+var issueStrike = function() {
+	life--;
+	console.log("Current life: " + life);
+	if (life === 0) {
+		gameOver = true;
+		console.log("sending lose");
+		io.sockets.emit('lose', { text: 'You lose!' });
+
+		// Reset the game
+		resetGame();
+	} else {
+		taskOver = true
+		taskseconds = 0;
+		console.log("task time out");
+		io.sockets.emit('health_down',{ text: 'Wrong! Your health now goes down by one.'});
+		console.log("health down");
+	}
 };
 
 // Generate a task
@@ -211,9 +236,23 @@ var taskParser = function (completeTask) {
 	specificTask = (taskComponents[3]).split(":");
 	lowerBoundDirection = parseInt(specificTask[1]);
 	specificTask = (taskComponents[4]).split(":");
-	timeLimit = parseInt(specificTask[1]);
+	timeLimit = (parseInt(specificTask[1]) * 10);
 	specificTask = (taskComponents[5]).split(":");
 	torpedoColor = specificTask[1];
+	
+	console.log("u_depth: " + upperBoundDepth);
+	console.log("l_depth: " + lowerBoundDepth);
+	console.log("u_direction: " + upperBoundDirection);
+	console.log("l_direction: " + lowerBoundDirection);
+	console.log("timeLimit: " + timeLimit);
+	console.log("torpedoColor: " + torpedoColor);
+	
+	// Get target depth, direction, and color.
+	var depth = ((upperBoundDepth + lowerBoundDepth) / 2);
+	var direction = ((upperBoundDirection + lowerBoundDirection) / 2);
+	var instruction = ("Depth: " + depth + "\nDirection: " + direction + "\nTorpedo: " + torpedoColor);
+	console.log("instruction: " + instruction);
+	io.sockets.emit('instruction', instruction);
 }
 
 // Launch the server.
